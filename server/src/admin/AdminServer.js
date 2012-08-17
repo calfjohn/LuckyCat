4018f49876554f62cfad9d020a5018f5c0ceb64a
yangws <yangws@cocos2d-x.org>
2012-08-17 10:31:00 +0800

    AS　用户登陆模块　http+socket验证框架ＯＫ。

--- server/src/admin/AdminServer.js
+++ server/src/admin/AdminServer.js
@@ -8,15 +8,20 @@ require("../system/Log");
 
 var http = require("http")
     ,express = require("express")
+    , adminServerCfg = require("../config/admin.AdminServer")
+    ,sessionStore = new express.session.MemoryStore
+    , sessionSecret = "LuckyCatAdmin"
+    ,parseCookie = express.cookieParser(sessionSecret)
     ,app = express()
     ,server = http.createServer(app)
     ,io = require("socket.io")
+    ,utils = require("util")
     ,log = new Log("AdminServer");
 
 app.configure(function() {
     app.use(express.bodyParser());    // This cause handler' on data function doesn't be called.
-    app.use(express.cookieParser());
-    app.use(express.session({secret:"anything", key:"express.sid"}));
+    app.use(parseCookie);
+    app.use(express.session({store: sessionStore, secret:sessionSecret, key:"express.sid"}));
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + "/www"));  //设定静态网页服务目录
@@ -63,12 +68,71 @@ app.start = function() {
 
 app.initHandlers = function (aExpress) {
     aExpress.post("/login", require("./handler/login"));
+    aExpress.get("/js/adminServer", function(req, res, next) {
+        res.setHeader("Content-Type", "text/javascript");
+        res.end(utils.format("var adminServer='%s';", req.headers.host));
+    });
+    aExpress.get("/*", aExpress.checkHttpAuthorization);
 
     // init socket handlers
-    var socket = io.listen(server, {log:false});
-    socket.sockets.on("connection", require("./socket/RoleAgent"));
+    var sio = io.listen(server, {log:false});
+    sio.set("authorization", aExpress.checkSocketAuthorization);
+    sio.sockets.on("connection", require("./socket/RoleAgent"));
 
-    aExpress.adminSocket = socket;
+    aExpress.sio = sio;
+};
+
+// 检查网页连接的授权，否则转到login.html页面
+app.checkHttpAuthorization = function (req, res, next) {
+    if (req.path == "/login.html") {
+        next();
+        return;
+    }
+    //log.d("checkHttpAuthorization(path=", req._parsedUrl.pathname, ",sessionID=", req.session.id, "):", req.session.token);
+    //使用request.session来判断是否登录
+    if( req.session.token && req.session.token!==''){
+        //需要判断token是否合法，合法证明已登录
+
+        next();
+    }else{
+        //读取登录页面，要求登录
+        //var realpath = __dirname + '/views/' + url.parse('login.html').pathname;
+        //var txt = fs.readFileSync(realpath);
+        //res.end(txt);
+        res.redirect("/login.html");
+    }
+};
+
+// 检查socket连接是否有授权，授权数据保存在cookie中
+app.checkSocketAuthorization = function (req, accept) {
+    var cookies = null;
+    parseCookie(req, null, function(err) {
+        if (! err) {
+            cookies = req.signedCookies;
+        }
+    });
+    log.d("checkSocketAuthorization(referer=", req.headers.referer, ",cookies=", cookies, ")");
+
+    // check if there's a cookie header
+    if (cookies && cookies['express.sid']) {
+        // 使用sid从sessionStore中获取session
+        sessionStore.load(cookies['express.sid'], function (err, session) {
+            // 如果获取失败，取消连接
+            if (err) {
+                accept(err.message, false);
+            } else {
+                // 把session挂到req上，以便在下面的事件中访问
+                req.session = session;
+                accept(null, true);
+            }
+        });
+    } else if (req.query.server_secret && (req.query.server_secret == adminServerCfg.server_secret)) {
+        // 从别的服务器连接过来的
+        accept(null, true);
+    } else {
+        // 连接失败
+        accept(null, false);
+    }
 };
 
 module.exports = app;
--- server/src/admin/handler/login.js
+++ server/src/admin/handler/login.js
@@ -2,9 +2,10 @@
 require("../../system/Log");
 
 module.exports = function(req, res, next) {
-    var name = req.body.name;
-    var password = req.body.password;
-    console.log("/login:" + req.body);
-    req.session.user = name;
+    var name = req.body.role_name;
+    var password = req.body.role_password;
+
+    // 验证用户名密码，最后成功的话给一个token
+    req.session.token = name;
     res.redirect("/");
 };
