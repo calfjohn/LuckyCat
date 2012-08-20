3ca7b522976277a29db6c65d610c0829c0968ad0
marucschoo <appdev@sina.com>
2012-08-20 10:11:24 +0800

    modify proj

---
+++ server/src/game/DictManager.js
@@ -0,0 +1,63 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: lihex
+ * Date: 12-8-16
+ * Time: 下午12:07
+ * To change this template use File | Settings | File Templates.
+ */
+
+if (global.DictManager) {
+    module.exports = DictManager;
+    return;
+}
+
+require("../system/DBAgent");
+require("../system/Log");
+require("./Actor");
+
+var log = new Log("DictManager");
+var util = require("util");
+
+DictManager = {
+    _dbAgent : null,
+    _cacheDictEquipment: null,
+    initInstance:function (dbConfig, callback) {
+        DictManager._dbAgent = new DBAgent(dbConfig);
+        DictManager._dbAgent.connect(true);
+        // Cache all equipment data on server start
+        DictManager._dbAgent.query("SELECT * FROM `dict_equipment`", function (err, rows) {
+            if (err) {
+                throw err;
+                return;
+            }
+            /**
+             *   DictManager._cacheDictEquipment struct:
+             *     |--id:key
+             *          |--data:value
+             */
+            DictManager._cacheDictEquipment = {};
+            for(var i = 0; i < rows.length; ++i){
+                var data = rows[i];
+                var id= "" + data.id;
+                DictManager._cacheDictEquipment[id] = data;
+            }
+        });
+
+        process.nextTick(function () {
+            callback(null);
+        });
+    },
+
+    getEquipmentByID: function(id, callback) {
+        //get a actor basic info by uuid
+        var equipment = DictManager._cacheDictEquipment[""+id];
+        if(undefined == equipment){
+            equipment = null;
+        }
+        callback(equipment);
+    }
+
+
+};
+
+module.exports = DictManager;
