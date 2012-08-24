1963e3898be94c1a5d2ab01399fc19a19b7e662c
yangws <yangws@cocos2d-x.org>
2012-08-24 11:55:55 +0800

    API reference页面 能显示module 的文档。

--- server/src/game/www/js/api_reference.js
+++ server/src/game/www/js/api_reference.js
@@ -1,6 +1,12 @@
 $(function(){
     $("#module_list").accordion({
         header: "h3",
-        fillSpace: true
+        fillSpace: true,
+        changestart: function(event, ui) {
+            var meta = document.getElementById("meta");
+            var namespace = meta.getAttribute("namespace");
+            var url = "/" + namespace + "/" + ui.newContent.context.outerText.replace(/[\r\n]/g, "");
+            document.getElementById("content").setAttribute("src", url);
+        }
     });
 });
