61a49760a7270a08b3199db805299bf592c3b0ba
ibaozi <hujiang@cocos2d.org>
2012-08-27 17:14:50 +0800

    add custom scrollview class

---
+++ client/Classes/PlayerInfo/CustomCCScrollView.cpp
@@ -0,0 +1,24 @@
+//
+//  CustomCCScrollView.cpp
+//  LuckyCat
+//
+//  Created by Cocos2d-x on 12-8-27.
+//  Copyright (c) 2012年 厦门雅基软件有限公司. All rights reserved.
+//
+
+#include "CustomCCScrollView.h"
+
+CustomCCScrollView::CustomCCScrollView(){
+
+}
+CustomCCScrollView::~CustomCCScrollView(){
+
+}
+
+bool CustomCCScrollView::init(){
+    CCScrollView::init();
+    return true;
+}
+void CustomCCScrollView::registerWithTouchDispatcher(){
+    CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, -200, false);
+}
