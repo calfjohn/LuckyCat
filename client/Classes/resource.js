ad88e8f50d1472d618bd3bf124668322345dafa9
lihex <hexuan.li@cocos2d-x.org>
2012-09-04 10:12:45 +0800

    完成html5工程配置
    开发html5流程
    1. 编写的js文件放如Classes目录
    2. 编辑html5/cocos2d.js,在appFiles中增加新编写的js文件相对路径

---
+++ client/Classes/resource.js
@@ -0,0 +1,20 @@
+var s_HelloWorld = "../Resources/HelloWorld.png";
+var s_CloseNormal = "../Resources/CloseNormal.png";
+var s_CloseSelected = "../Resources/CloseSelected.png";
+
+var g_ressources = [
+    //image
+    {type:"image", src:s_HelloWorld},
+    {type:"image", src:s_CloseNormal},
+    {type:"image", src:s_CloseSelected}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
