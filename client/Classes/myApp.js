1777b2ff35ec6ce2a695be85e2a0338e5cd2fef9
ibaozi <hujiang@cocos2d.org>
2012-09-11 11:50:06 +0800

    delete net mgr class

--- client/Classes/myApp.js
+++ client/Classes/myApp.js
@@ -78,10 +78,91 @@ var MyLayer = cc.Layer.extend({
 
         lazyLayer.addChild(this.sprite, 0);
 
+        var url = "http://192.168.0.51:22222/game/pk/getPKList/";
+        var parameter = "{\"header\":{\"token\": \"3\", \"index\": 1}, \"meta\":{\"mod\": \"pk\", \"do\": \"getPKList\", \"ver\":1, \"in\":{}}}"
+        sendAsynchronRequest(url,parameter,loadCallBack);
+
         return true;
     }
 
 });
+
+var xmlHttp;
+function createXMLHttpRequest(){
+    //Mozilla 浏览器（将XMLHttpRequest对象作为本地浏览器对象来创建）
+    if(window.XMLHttpRequest){ //Mozilla 浏览器
+        xmlHttp = new XMLHttpRequest();
+    }else if(window.ActiveXObject) { //IE浏览器
+        //IE浏览器（将XMLHttpRequest对象作为ActiveX对象来创建）
+        try{
+            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+        }catch(e){
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }catch(e){}
+        }
+    }
+    if(xmlHttp == null){
+        alert("不能创建XMLHttpRequest对象");
+        return false;
+    }
+}
+//用于发出异步请求的方法
+function sendAsynchronRequest(url,parameter,callback){
+    createXMLHttpRequest();
+    if(parameter == null){
+        //设置一个事件处理器，当XMLHttp状态发生变化，就会出发该事件处理器，由他调用
+        //callback指定的javascript函数
+        xmlHttp.onreadystatechange = callback;
+        //设置对拂去其调用的参数（提交的方式，请求的的url，请求的类型（异步请求））
+        xmlHttp.open("GET",url,true);//true表示发出一个异步的请求。
+        xmlHttp.send(null);
+    }else{
+        xmlHttp.onreadystatechange = callback;
+        xmlHttp.open("POST",url,true);
+        xmlHttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        //xmlHttp.setRequestHeader("Access-Control-Allow-Origin","*");
+        xmlHttp.send(parameter);
+    }
+}
+
+// 指定回调方法
+function loadCallBack(){
+    try
+    {
+        if (xmlHttp.readyState == 4) {
+            if (xmlHttp.status == 200) {
+                if(xmlHttp.responseText != null && xmlHttp.responseText != ""){
+                    var divProid = document.getElementById('videolist');
+                    divProid.innerHTML = xmlHttp.responseText;
+                    for(i=0;i<len;i++)
+                    {
+                        var video_url = document.getElementById("videolist"+i+"").href;
+                        if(video_url != undefined && video_url != null && video_url != ""){
+                            window.location.href = video_url;
+                        }
+                    }
+                }
+            }
+        }
+        if (xmlHttp.readyState == 1)
+        {
+            alert("正在加载连接对象......");
+        }
+        if (xmlHttp.readyState == 2)
+        {
+            alert("连接对象加载完毕。");
+        }
+        if (xmlHttp.readyState == 3)
+        {
+            alert("数据获取中......");
+        }
+    }
+    catch (e)
+    {
+        //alert(e);
+    }
+}
 
 var MyScene = cc.Scene.extend({
     onEnter:function () {
