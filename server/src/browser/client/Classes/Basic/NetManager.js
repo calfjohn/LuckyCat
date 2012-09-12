8d63f9c7dde92c8753c27ca32c9adf471a4fdbf6
ibaozi <hujiang@cocos2d.org>
2012-09-12 15:41:19 +0800

    modify netmgr class , add generate url and json data function;

--- server/src/browser/client/Classes/Basic/NetManager.js
+++ server/src/browser/client/Classes/Basic/NetManager.js
@@ -3,6 +3,9 @@ var lc = lc = lc || {};
 g_sharedNetManager = null;
 
 lc.NetManager = cc.Class.extend({
+    m_iToken:3,
+    m_strURL:"http://localhost:22222/game/",
+    m_iIndex:1,
     _init:function(){
 
     },
@@ -22,13 +25,15 @@ lc.NetManager = cc.Class.extend({
             }
         }
         if(xmlhttp == null){
-            alert("不能创建XMLHttpRequest对象");
+            //alert("不能创建XMLHttpRequest对象");
             return false;
         }
         return xmlhttp;
     },
-    sendRequest:function(url,parameter,responseCallback,errorCallback){
+    sendRequest:function(modEnum,doEnum,requestData,responseCallback,errorCallback){
         var xhr = this.createXMLHttpRequest();
+        var url = this.generatePostURL(modEnum,doEnum);
+        var parameter = this.generatePostJsonData(modEnum,doEnum,requestData);
         if(parameter == null){
             //设置一个事件处理器，当XMLHttp状态发生变化，就会出发该事件处理器，由他调用
             //callback指定的javascript函数
@@ -52,8 +57,21 @@ lc.NetManager = cc.Class.extend({
             };
             xhr.open("POST",url,true);
             xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded;");
+            cc.log(parameter);
             xhr.send(parameter);
         }
+    },
+    generatePostJsonData:function(modEnum,doEnum,requestData){
+        var jsonText;
+        if(requestData == null || requestData == ""){
+            jsonText = "{\"header\":{\"token\": \"" + this.m_iToken + "\", \"index\": " + (this.m_iIndex++) + "}, \"meta\":{\"mod\": \"" + g_modNames[modEnum] + "\", \"do\": \"" + g_doNames[doEnum] + "\", \"ver\":" + g_doVersion[doEnum] + ", \"in\":{}}}";
+        }else{
+            jsonText = "{\"header\":{\"token\": \"" + this.m_iToken + "\", \"index\": " + (this.m_iIndex++) + "}, \"meta\":{\"mod\": \"" + g_modNames[modEnum] + "\", \"do\": \"" + g_doNames[doEnum] + "\", \"ver\":" + g_doVersion[doEnum] + ", \"in\":{" + requestData + "}}}";
+        }
+        return jsonText;
+    },
+    generatePostURL:function(modEnum,doEnum){
+        return this.m_strURL + g_modNames[modEnum]+"/"+g_doNames[doEnum]+"/";
     }
 });
 
